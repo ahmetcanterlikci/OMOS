966897af9fd2391f0cf989e749a1601b164b93ef
Ahmet Can Terlikçi <ahmetcan.22@hotmail.com>
2020-01-02 17:26:46 +0300

    Second draft update.

--- OMOS/omosapp/urls.py
+++ OMOS/omosapp/urls.py
@@ -5,4 +5,18 @@ from omosapp import views
 urlpatterns = [
     path('login', views.login_view, name='login_view'),
     path('home', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('account', views.account, name='account'),
+    path('addresses', views.addresses, name='addresses'),
+    path('changepassword', views.changepassword, name='changepassword'),
+    path('chart', views.chart, name='chart'),
+    path('clientinfo', views.clientinfo, name='clientinfo'),
+    path('clientorders', views.clientorders, name='clientorders'),
+    path('forgetpassword', views.forgetpassword, name='forgetpassword'),
+    path('search', views.search, name='search'),
+    path('myclients', views.myclients, name='myclients'),
+    path('myorders', views.myorders, name='myorders'),
+    path('myprofile', views.myprofile, name='myprofile'),
+    path('order', views.order, name='order'),
+
 ]
--- OMOS/omosapp/views.py
+++ OMOS/omosapp/views.py
@@ -12,6 +12,58 @@ def home(request):
     return render(request, 'omosapp/index.html', {})
 
 
+def register(request):
+    return render(request, 'omosapp/register.html', {})
+
+
+def account(request):
+    return render(request, 'omosapp/account.html', {})
+
+
+def addresses(request):
+    return render(request, 'omosapp/addresses.html', {})
+
+
+def changepassword(request):
+    return render(request, 'omosapp/changePassword.html', {})
+
+
+def chart(request):
+    return render(request, 'omosapp/chart.html', {})
+
+
+def clientinfo(request):
+    return render(request, 'omosapp/clientinfo.html', {})
+
+
+def clientorders(request):
+    return render(request, 'omosapp/clientorders.html', {})
+
+
+def forgetpassword(request):
+    return render(request, 'omosapp/forgetPassword.html', {})
+
+
+def search(request):
+    return render(request, 'omosapp/restaurant.html', {})
+
+
+def myclients(request):
+    return render(request, 'omosapp/myclients.html', {})
+
+
+def myorders(request):
+    return render(request, 'omosapp/myorders.html', {})
+
+
+def myprofile(request):
+    return render(request, 'omosapp/myprofile.html', {})
+
+
+def order(request):
+    return render(request, 'omosapp/order.html', {})
+
+
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
